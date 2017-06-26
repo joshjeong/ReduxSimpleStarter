5412aaf758cea13463997bed3029a2176957f883
Josh Jeong <jcjeong@gmail.com>
2017-06-26 12:22:56 -0700

    asdf

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,11 @@
+// reducers have two arguments: state and action
+// state is not application state, but only state the
+// reducer is responsible for
+export default function(state = null, action) {
+    switch(action.type) {
+        case 'BOOK_SELECTED':
+            return action.payload;
+    }
+
+    return state;
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+     return [
+         { title: 'Javascript the good parts', pages: 10 },
+         { title: 'Harry Potter', pages: 110 },
+         { title: 'Something something', pages: 40 },
+         { title: 'Red Teaming', pages: 15 }
+     ];
+}
